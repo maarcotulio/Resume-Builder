3440f7b88e2eb1e5e3a0e026696c3e41fc281099
maarcotulio <marcotuliotavaresoliveira@gmail.com>
2025-03-12 17:24:17 -0300

    style: improved the loading and response

--- src/app/dash/components/TextArea.tsx
+++ src/app/dash/components/TextArea.tsx
@@ -20,42 +20,47 @@ export function TextArea({
 
   return (
     <>
-      <textarea
-        value={skills}
-        onChange={(e) => setSkills(e.target.value)}
-        className="w-full bg-transparent h-auto border-2 border-white p-4 rounded-lg"
-        placeholder="Write here your skills"
-      />
-      <div className="flex w-full items-center justify-between h-auto">
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          className="bg-transparent p-2 border-white border-2 rounded-md"
-          onChange={(e) => setName(e.target.value)}
+      <div className="w-full">
+        <div className="grid grid-cols-3 gap-4">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            className="bg-transparent p-2 border-white border-2 rounded-md "
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Job"
+            value={job}
+            className="bg-transparent p-2 border-white border-2 rounded-md"
+            onChange={(e) => setJobType(e.target.value)}
+          />
+          <input
+            type="number"
+            value={experienceYears}
+            placeholder="Years of experience"
+            className="bg-transparent p-2 border-white border-2 rounded-md"
+            onChange={(e) => setExperienceYears(Number(e.target.value))}
+            min={0}
+          />
+        </div>
+        <textarea
+          value={skills}
+          onChange={(e) => setSkills(e.target.value)}
+          className="w-full bg-transparent h-auto border-2 border-white p-4 rounded-lg my-4"
+          placeholder="Write here your skills"
         />
-        <input
-          type="text"
-          placeholder="Job"
-          value={job}
-          className="bg-transparent p-2 border-white border-2 rounded-md"
-          onChange={(e) => setJobType(e.target.value)}
-        />
-        <input
-          type="number"
-          value={experienceYears}
-          placeholder="Years of experience"
-          className="bg-transparent p-2 border-white border-2 rounded-md"
-          onChange={(e) => setExperienceYears(Number(e.target.value))}
-          min={0}
-        />
-
-        <button
-          className="p-2 rounded-md bg-zinc-500 hover:bg-zinc-600"
-          onClick={() => handleGenerate({ name, job, skills, experienceYears })}
-        >
-          Generate
-        </button>
+        <div className="flex justify-end items-center">
+          <button
+            className="p-2 rounded-md bg-zinc-500 hover:bg-zinc-600 "
+            onClick={() =>
+              handleGenerate({ name, job, skills, experienceYears })
+            }
+          >
+            Generate
+          </button>
+        </div>
       </div>
     </>
   );
--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -3,13 +3,32 @@
 import { LoaderCircle } from "lucide-react";
 import { trpc } from "../_trpc/client";
 import { TextArea } from "./components/TextArea";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { PromptProps } from "@/types/Prompt";
 
 export default function Home() {
   const [response, setResponse] = useState();
   const { mutateAsync, isLoading } = trpc.chat.useMutation();
+
+  const contentRef = useRef("");
+
+  const handleDownloadPdf = async () => {
+    const element = contentRef.current;
+    if (!element) return;
+
+    const html2pdf = (await import("html2pdf.js")).default;
+
+    const opt = {
+      margin: 0.5,
+      filename: "exported-document.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    };
+
+    html2pdf().set(opt).from(element).save();
+  };
 
   const handleGenerate = async ({
     name,
@@ -31,11 +50,20 @@ export default function Home() {
           <TextArea handleGenerate={handleGenerate} />
         </div>
       )}
-      {isLoading && <LoaderCircle className="animate-spin" />}
+      {isLoading && <LoaderCircle className="animate-spin" size={48} />}
 
-      {!!response && (
-        <div className="p-8 mt-8 w-1/2 margin-auto">
-          <ReactMarkdown>{response}</ReactMarkdown>
+      {!!response && isLoading && (
+        <div className="flex justify-center flex-col items-center w-1/2 gap-2 my-8">
+          <h1 className="text-2xl font-bold">Resume</h1>
+          <div>
+            <ReactMarkdown>{response}</ReactMarkdown>
+          </div>
+          <button
+            onClick={handleDownloadPdf}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Download PDF
+          </button>
         </div>
       )}
     </>
